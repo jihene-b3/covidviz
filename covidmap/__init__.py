871bb72517d626a935e8a4f27ea29ba16bd087b3
Chloe Serre-Combe <chloe.srcb@gmail.com>
2021-03-16 23:25:53 +0100

    Add a html map and a function get_cases
    get_cases does'nt get the right number for the moment

--- covidmap/__init__.py
+++ covidmap/__init__.py
@@ -1,3 +1,5 @@
 __version__ = '0.0.1'
 
 from .io.load_db import Load_db
+from .vis.plot_map import plot_map
+from .preprocess.get_cases import get_cases_by_dep
---
+++ covidmap/vis/plot_map.py
@@ -0,0 +1,10 @@
+import pygal                                                       
+from pygal.maps.fr import aggregate_regions
+
+def plot_map(cases):
+  fr_chart = pygal.maps.fr.Departments(human_readable=True)
+  fr_chart.title = 'Covid in France'
+  fr_chart = pygal.maps.fr.Departments()
+  
+  fr_chart.add('Covid cases', cases.to_dict())
+  fr_chart.render_in_browser()
