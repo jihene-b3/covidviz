98fd07a465aad9cdcf1d5ea78b3c5ffffce70f79
Chloe Serre-Combe <chloe.srcb@gmail.com>
2021-03-16 23:25:53 +0100

    Add a html map and a function get_cases
    get_cases does'nt get the right number for the moment

---
+++ covidmap/preprocess/get_cases.py
@@ -0,0 +1,13 @@
+import  pandas as pd
+import numpy as np
+
+# TO IMPROVE
+# doesn't show the right number of cases for the moment
+# because of the groupby
+
+# return a dataframe with the number of covid cases by departments
+def get_cases_by_dep(df_covid, log_scale=True):
+  cases_dep = df_covid.groupby(['maille_code']).size()
+  if log_scale:
+    cases_dep = np.log(cases_dep)
+  return cases_dep
