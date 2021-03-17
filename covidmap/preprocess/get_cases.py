5d4f21672d7d9dfc9a17b543cb464e83cf695e35
jihene-b3 <78120230+jihene-b3@users.noreply.github.com>
2021-03-17 20:50:58 +0100

    elements for get_cas_by_dep() function

--- covidmap/preprocess/get_cases.py
+++ covidmap/preprocess/get_cases.py
@@ -6,6 +6,12 @@ import numpy as np
 # because of the groupby
 
 # return a dataframe with the number of covid cases by departments
+## ideas  
+# create a function with a for loop that iterates over the list of elements in column "maille-code" : then df_covid["maille_code"=="DEP-"+ str(i)] 
+# or use df_covid[df_covid.maille_code.str.contains('Dep-'+ str(i))]
+# issue to think about : DEP-2A and DEP-2B 
+# maybe it would be easier to list the elements of the column maille_nom, then do the iteration over the list of characters of all french departments.
+
 def get_cases_by_dep(df_covid, log_scale=True):
   cases_dep = df_covid.groupby(['maille_code']).size()
   if log_scale:
