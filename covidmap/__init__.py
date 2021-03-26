501b0773fc29a1feb8209d8401f05966d3fd154b
Chloe Serre-Combe <chloe.srcb@gmail.com>
2021-03-26 11:24:21 +0100

    Add format_dep fonction in map_html.py

--- covidmap/__init__.py
+++ covidmap/__init__.py
@@ -3,3 +3,4 @@ __version__ = '0.0.1'
 from .io.load_db import Load_db
 from .vis.plot_map import plot_map
 from .preprocess.get_cases import get_cases_by_dep
+from .preprocess.format_dep import format_dep
