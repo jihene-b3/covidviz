6e8bb84166f6d324b2410ed6f1c8ee9514df0fdf
Zakaria Laabsi <52045850+OmegaZak@users.noreply.github.com>
2021-04-16 14:33:14 +0200

    Update dynamic visualisations

--- dashboard/dashapp.py
+++ dashboard/dashapp.py
@@ -5,8 +5,6 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
 
-#Load the data
-
 df = pd.read_csv('df_vacc.csv', index_col=0, parse_dates=True)
 df.index = pd.to_datetime(df['date'])
 
@@ -29,20 +27,17 @@ def get_options(list_location):
     dict_list.append({'label': i, 'value': i})
   return dict_list
 
-# Initialization of the dash app
 
 app = dash.Dash(__name__)
 
-# Define the dash app
-
 app.layout = html.Div(
     children=[
-        html.Div(className='row', #Define the row element
+        html.Div(className='row',
                  children=[
-                    html.Div(className='four columns div-user-controls', #Left element
+                    html.Div(className='four columns div-user-controls',
                              children=[
-                                 html.H2('COVID DASHBOARD'),
-                                 html.P('Visualising time series with Plotly - Dash.'),
+                                 html.H2('COVID VACCINES TRACKER FRANCE'),
+                                 html.P('Tracking vaccination rates by French department'),
                                  html.P('Pick one or more location from the dropdown below.'),
                                  html.Div(className='div-for-dropdown',
                                           children=[
@@ -56,16 +51,46 @@ app.layout = html.Div(
                                           style={'color': '#1E1E1E'})
                                  ]
                              ),
-                    html.Div(className='eight columns div-for-charts bg-grey', #Right element
+                    html.Div(className='eight columns div-for-charts bg-grey',
                              children=[
     dcc.Graph(id='timeseries',
               config={'displayModeBar': False},
-              animate=True,
-              figure=fig)
+              animate=True)
 ])
                     
                     ])
                               ])
-# Run the dash app
+
+# Callback for timeseries number of vaccinations
+@app.callback(Output('timeseries', 'figure'),
+              [Input('locselector', 'value')])
+def update_graph(selected_dropdown_value):
+    trace1 = []
+    df_sub = df
+    for codelocation in selected_dropdown_value:
+        trace1.append(go.Scatter(x=df_sub[df_sub['codelocation'] == codelocation].index,
+                                 y=df_sub[df_sub['codelocation'] == codelocation]['weekly'],
+                                 mode='lines',
+                                 opacity=0.7,
+                                 name= codelocation,
+                                 textposition='bottom center'))
+    traces = [trace1]
+    data = [val for sublist in traces for val in sublist]
+    figure = {'data': data,
+              'layout': go.Layout(
+                  colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
+                  template='plotly_dark',
+                  paper_bgcolor='rgba(0, 0, 0, 0)',
+                  plot_bgcolor='rgba(0, 0, 0, 0)',
+                  margin={'b': 15},
+                  hovermode='x',
+                  autosize=True,
+                  title={'text': 'Vaccinations', 'font': {'color': 'white'}, 'x': 0.5},
+                  xaxis={'range': [df_sub.index.min(), df_sub.index.max()]},
+              ),
+
+              }
+
+    return figure
 
 app.run_server(debug=True)
