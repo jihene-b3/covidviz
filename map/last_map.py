f4b1da5b67cde5ce7247063cf5b6e102244de738
Chloe Serre-Combe <chloe.srcb@gmail.com>
2021-04-17 12:01:40 +0200

    Arrange architecture and remove useless files

--- map/last_map.py
+++ map/last_map.py
@@ -1,5 +1,8 @@
 #%%
 import pydeck as pdk
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..'))
 import covidmap as cm
 import pandas as pd
 import geopandas as gpd
