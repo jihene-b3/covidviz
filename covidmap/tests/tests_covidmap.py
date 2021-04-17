bcaadaecad38d2e50719be02d61bcd221163ae10
Chloe Serre-Combe <chloe.srcb@gmail.com>
2021-04-17 16:21:21 +0200

    Update tests

--- covidmap/tests/tests_covidmap.py
+++ covidmap/tests/tests_covidmap.py
@@ -3,14 +3,16 @@ import sys
 import numpy as np
 import pandas as pd
 import warnings
-import matplotlib.pyplot as plt
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..')*2)
 
 import covidmap as cm
 
-def test_format_dep():
+def test_format_granularity():
     df_covid = pd.read_csv("../data/data_covid_clean.csv")
-    test = cm.format_dep(df_covid)
-    assert test["maille_code"][0] == "33"
+    test_reg = cm.format_granularity(df_covid, "region")
+    test_dep = cm.format_granularity(df_covid, "departement")
+    assert (test_reg["maille_code"][0] == "11" and test_dep["maille_code"][0] == "33")
+
+
