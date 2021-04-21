a78dadbb32f26edf210758a5071c73350efa076a
Chloe Serre-Combe <chloe.srcb@gmail.com>
2021-04-21 17:42:17 +0200

    Arrange screening functions

--- script/screening.py
+++ script/screening.py
@@ -5,15 +5,28 @@ import numpy as np
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..'))
 import covidviz as cvz
 
-#%%
-screening_daily = pd.read_csv('../covidviz/data/sp-pos-quot-dep-2021-04-16-19h05.csv', delimiter=';', low_memory=False)
 
 #%%
-screening_daily.rename(columns={'cl_age90' : 'cl_age', 'jour' : 'date', 'T' : 'Tests number', 'P' : 'Positive tests'}, inplace=True)
+grand_public_path = '../covidviz/data/scr_public_centers.csv'
+acces_restreint_path = '../covidviz/data/scr_private_centers.csv'
+depis_grand_public = pd.read_csv(grand_public_path)
+depis_acces_restreint = pd.read_csv(acces_restreint_path)
+dep_fr = pd.read_csv('../covidviz/data/depart_fr_coord.csv', delimiter=';')
+
 
 #%%
-DEP = pd.DataFrame(screening_daily['dep'].unique())
-DEP.set_axis(['code'], axis=1, inplace=True)
+screening_daily = pd.read_csv(
+    '../covidviz/data/sp-pos-quot-dep-2021-04-16-19h05.csv',
+    delimiter=';',
+    low_memory=False)
+
+screening_daily.rename(
+    columns={'cl_age90': 'cl_age',
+             'jour': 'date',
+             'T': 'Tests number',
+             'P': 'Positive tests'},
+    inplace=True)
 
 #%%
+
 cvz.daily_test(90,'29')
--- script/sparse_mat.py
+++ script/sparse_mat.py
@@ -5,4 +5,29 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..'))
 import covidviz as cvz
 import pandas as pd
+from scipy import sparse
+import networkx as nx
+import matplotlib.pyplot as plt
 
+#%%
+
+G = nx.Graph() # creation du graphe
+# les aretes : avec eventuellement des poids
+# si pas de poids c'est 0 ou 1 (= si ya une arete)
+
+G.add_edge('Patient', 'B', weight=4)
+G.add_edge('B', 'D', weight=2)
+G.add_edge('Patient', 'C', weight=3)
+G.add_edge('C', 'D', weight=4)
+G.add_edge('D', 'Patient', weight=2)
+
+# %%
+pos = nx.spring_layout(G, seed=1234)
+nx.draw_networkx(G, pos, with_labels=True)
+
+labels_edge = nx.get_edge_attributes(G, "weight")
+nx.draw_networkx_edge_labels(G, pos, edge_labels=labels_edge)
+nx.draw_networkx_edges(G, pos, width=list(labels_edge.values()))
+plt.axis('off')
+plt.show()
+# %%
