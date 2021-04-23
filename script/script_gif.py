a08dd5e4c7f735930aea1f659e46c305e7689e20
jihene-b3 <78120230+jihene-b3@users.noreply.github.com>
2021-04-23 20:14:06 +0200

    improve functions and add tests (in progress)

--- script/script_gif.py
+++ script/script_gif.py
@@ -19,6 +19,7 @@ _data_department = cvz.adapt_time(_data_department)
 
 _data_region = cvz.choose_granularity(_data, 'region')
 _data_region = cvz.adapt_time(_data_department)
+
 #%%
 
 df_clean_dep_death = cvz.data_treatment_by_option(_data_department, "deces")
@@ -27,3 +28,19 @@ cvz.plot_animation(df_clean_dep_death, "departement", "deces")
 
 
 #%%
+
+df_clean_dep_cas_confirmes = cvz.data_treatment_by_option(_data_department, "cas_confirmes")
+
+cvz.plot_animation(df_clean_dep_cas_confirmes, "departement", "cas_confirmes")
+#%%
+
+
+#%%
+df_clean_region_cas_deces = cvz.data_treatment_by_option(_data_department, "deces")
+
+cvz.plot_animation(df_clean_region_cas_confirmes, "departement", "deces")
+
+#%%
+df_clean_region_cas_deces = cvz.data_treatment_by_option(_data_department, "cas_confirmes")
+
+cvz.plot_animation(df_clean_region_cas_confirmes, "departement", "cas_confirmes")
