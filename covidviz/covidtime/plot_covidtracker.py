b9008612348727edd1aa218eda24c5400ed08095
jihene-b3 <78120230+jihene-b3@users.noreply.github.com>
2021-04-24 16:44:04 +0200

    add script_covidtracker.py

---
+++ covidviz/covidtime/plot_covidtracker.py
@@ -0,0 +1,36 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..'))
+import covidviz as cvz
+from covidviz import data 
+from covidviz.preprocess.clean_df import choose_columns, choose_granularity
+import matplotlib.pyplot as plt 
+import pandas as pd
+import matplotlib.ticker as mtick
+
+def ratio(gb_data, data_depcode, data_ratio_hospitalises,current_date, data_hospitalises, current_date_file, min_value_80p , nbhospitalises_80p) : 
+    fig, ax = plt.subplots(figsize=(12, 8))
+
+    plt.title(f"Ratio of in-hospital deaths to hospitalizations : {current_date}", fontsize=20)
+    plt.ylabel("Nombre cumulé de décès / Nombre cumulé d'hospitalisations")
+    plt.xlabel("Nombre cumulé de patients hospitalisés")
+
+    for i, txt in enumerate(data_depcode):
+        if (data_hospitalises[i] > data_hospitalises.max() * 0.20):
+            ax.annotate(txt, (data_hospitalises[i], data_ratio_hospitalises[i]), xytext=(data_hospitalises[i] + 20, data_ratio_hospitalises[i]))        
+
+    plt.axhline(data_ratio_hospitalises.mean(), color='green', linestyle='--', label=f'Ratio de décès moyen ({data_ratio_hospitalises.mean():.2f}%)')
+
+    plt.axvline(min_value_80p, color='pink', linestyle='-', label=f"80% du nb d'hospitalisés en France sont à droite de la ligne ({nbhospitalises_80p:.0f} hospitalisés)")
+
+    ax.scatter(data_hospitalises, data_ratio_hospitalises)
+
+    ax.annotate('updated chart',xy=(1, 0), xytext=(-15, 10), fontsize=15,
+        xycoords='axes fraction', textcoords='offset points',
+        bbox=dict(facecolor='white', alpha=0.9),
+        horizontalalignment='right', verticalalignment='bottom')
+
+    ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.0f%%'))
+    plt.legend()
+
+    current_date_file = gb_data['date'].max().strftime('%Y%m%d')
+    plt.show()
--- script/script_covidtracker.py
+++ script/script_covidtracker.py
@@ -4,9 +4,36 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..'
 import covidviz as cvz
 from covidviz import data 
 from covidviz.preprocess.clean_df import choose_columns, choose_granularity
+import matplotlib.pyplot as plt 
 import pandas as pd
+import matplotlib.ticker as mtick
+from covidviz.covidtime.plot_covidtracker import ratio
+
 
 # %%
-data = cvz.choose_columns(cvz.Load_db.save_as_df(), ['date', 'granularite', 'maille_code', 'maille_nom', 'reanimation','deces', 'cas_confirmes','gueris','source_nom'])
+a = cvz.Load_db.save_as_df()
+data = cvz.choose_columns(a, ['date', 'granularite', 'maille_code', 'maille_nom', 'reanimation','deces', 'cas_confirmes','gueris','hospitalises','source_nom'])
 
 # %%
+data = cvz.choose_granularity(data, 'departement')
+
+# %%
+data['date'] = pd.to_datetime(data['date'])
+
+# %%
+gb_data= data[data['date'] == data['date'].max()].reset_index()
+current_date = data['date'].max().strftime('%d/%m/%Y')
+current_date_file = gb_data['date'].max().strftime('%d/%m/%Y')
+#hospitalisees total = hospitalises + gueris
+data_ratio_hospitalises = (gb_data['deces'] / (gb_data['hospitalises'] + gb_data['gueris'])) * 100
+data_hospitalises = gb_data['hospitalises'] + gb_data['gueris']
+data_deces = gb_data['deces']
+data_depcode = gb_data['maille_nom']
+
+# %%
+nbhospitalises_80p = data_hospitalises.sum() * 0.80
+min_value_80p = data_hospitalises.loc[data_hospitalises[data_hospitalises.sort_values(ascending=False).cumsum() <= nbhospitalises_80p].index].min()
+
+# %%
+
+ratio(gb_data, data_depcode, data_ratio_hospitalises,current_date, data_hospitalises, current_date_file, min_value_80p , nbhospitalises_80p) : 
