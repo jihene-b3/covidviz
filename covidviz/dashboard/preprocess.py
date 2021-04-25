70a0b7267e61021b1753e2f181ae44f376ce3da3
Zakaria Laabsi <52045850+OmegaZak@users.noreply.github.com>
2021-04-25 23:14:00 +0200

    Update preprocess.py

--- covidviz/dashboard/preprocess.py
+++ covidviz/dashboard/preprocess.py
@@ -1,7 +1,16 @@
 
 def preprocess(df,gdf):
+    """ Does the processing by concatenating a dataset and a geodataframe with the index(department code for example) in common. 
+    This gives a Pandas dataframe containing for each date a list of departments with their associated geometric contours 
     
-    df_vacsi = pd.read_csv(vacsi_dep)
+    :param df: dataset (CSV file)
+    :param gdf: GeoJSON file
+    :type df : string
+    :type gdf : string
+    """
+
+    
+    df_vacsi = pd.read_csv(df)
     df_vacsi = df_vacsi[1:]
     df_vacsi['date'] = pd.to_datetime(df_vacsi['date'])
     df_vacsi.head(10)
@@ -17,23 +26,21 @@ def preprocess(df,gdf):
         temp_df[i] = df_vacsi.loc[ df_vacsi['date'] == i]
     dict_df = dict()
     for key, df in temp_df.items() :
-        dict_df[key] = df.groupby('code').max().drop(['codelocation','where','Unnamed: 0'], axis=1)
-    
-    dict_df[max].head()
+        dict_df[key] = df.groupby('code').max().drop(['codelocation','where','Unnamed: 0'], axis=1
 
 
 
-    gdf  = 'departements-avec-outre-mer.geojson'
+    #gdf  = 'departements-avec-outre-mer.geojson'
     f = gpd.read_file(gdf)
     f = f.set_index('code')
     f = f.sort_index(axis=0)
 
-    f.head()
+    
 
     dict_vaccin = dict()
     for i in df_jour :
         dict_vaccin[i] = pd.concat([f,dict_df[i]], axis = 1)
-    dict_vaccin[max].head(1000)
+    
 
     ts_vacc = pd.concat(dict_vaccin, axis=0)
     ts_vacc.sort_index(ascending=True)
