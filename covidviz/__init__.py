5571d5a4e67b0c6872343d90a1488aa9edee17c1
Chloe Serre-Combe <chloe.srcb@gmail.com>
2021-04-25 08:49:09 +0200

    Update report and correct some errors

--- covidviz/__init__.py
+++ covidviz/__init__.py
@@ -10,10 +10,10 @@ from .covidtime.time_gif import adapt_time, data_treatment_by_option, plot_anima
 from .icu.region import clean_df_reg, regroup_by_reg, create_df_all_reg, icu_reg_all, icu_reg_display, icu_by_reg_all, icu_by_reg_display, icu_all_reg_display, change_format_reg, create_reg_total, icu_reg_repartition, create_icu_beds_reg, heat_map_icu_reg
 from .screening.graphs import screening_by_age_dep, screening_by_age, screening_by_dep, daily_test, daily_test_age, daily_test_dep
 from .sparse.transfer_graph import create_transfer_graph, plot_transfer_graph, plot_adjacency_matrix
-from covidviz.preprocess.format_data import clean_gender,clean_age,format_age,remove_nan,group,enable_time_series_plot
-from covidviz.demographyfactors.utils_plot import df_plot_dec,df_plot_rea,df_plot_hosp,df_plot_gender
-from covidviz.covidtime.viz_epidemio import data_preparation,data_preproc,plot_field_loops, axis_date_limits,enable_time_series_plot
-from covidviz.covidtime.plot_covidtracker import ratio
+from .preprocess.format_data import clean_gender,clean_age,format_age,remove_nan,group,enable_time_series_plot
+from .demographyfactors.utils_plot import df_plot_dec,df_plot_rea,df_plot_hosp,df_plot_gender
+from .covidtime.viz_epidemio import data_preparation,data_preproc,plot_field_loops, axis_date_limits,enable_time_series_plot
+from .covidtime.plot_covidtracker import ratio
 from .sparse.transfer_graph import create_transfer_graph, plot_transfer_graph, plot_adjacency_matrix
 from .screening.plot_map import clean_public_centers, clean_dep, map_dep, regroup_map, regroup_public_center_by_dep, clean_private_centers, regroup_private_center_by_dep, markers_set, map_screening
 
